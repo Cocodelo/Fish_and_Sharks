0977a12c2bfcc867c2c7103b72ce4c6de33fe438
Cocodelo <cocodelo.fouque@gmail.com>
2025-10-25 12:35:41 +0200

    Update Main_File.py

--- Main_File.py
+++ Main_File.py
@@ -7,7 +7,7 @@ def copyMat(m):
 
 colorTable = [(0, 0, 102),(102, 255, 51),(0, 102, 255)]
 
-class Ocean :
+class Ocean :   
 
     def __init__(self,width=50,height=50):
         self.width,self.height = width,height
@@ -32,96 +32,78 @@ class Ocean :
     def initialize(self):
         for i in range(0,self.height):
             for j in range(0,self.width):
-                r=rd.randint(0,9)
-                if r<=0:
-                    self.grid[i][j]=Shark(i,j)
-                    self.animals.append(self.grid[i][j])
-                elif r>=6:
-                    self.grid[i][j]=Fish(i,j)
-                    self.animals.append(self.grid[i][j])
+                if rd.randint(0,9)<=0:
+                    self.grid[i][j]=Shark(1,2)
+                    self.animals.append(Shark)
+                elif rd.randint(0,9)>=6:
+                    self.grid[i][j]=Fish(1,2)
+                    self.animals.append(Fish)
         self.states.append(copyMat(self.grid))
         self.animalCount.append(self.count())
 
     def show(self):
-         # used for the animation do not touch !
-         fig = plt.figure(1)
-         mapsList = []
-         for k in range(len(self.states)):
-             for i in range(self.width):
-                 for j in range(self.height):
-                     if self.states[k][i][j] == None :
-                         self.states[k][i][j] = colorTable[0]
-                     else :
-                         self.states[k][i][j] = colorTable[self.states[k][i][j].num]
-             img = plt.matshow(self.states[k],fignum=1)
-             mapsList.append([img])
-         anim = animation.ArtistAnimation(fig,mapsList,interval=100,blit=True,repeat=False)
-         plt.show()
-         
+        # used for the animation do not touch !
+        fig = plt.figure(1)
+        mapsList = []
+        for k in range(len(self.states)):
+            for i in range(self.width):
+                for j in range(self.height):
+                    if self.states[k][i][j] == None :
+                        self.states[k][i][j] = colorTable[0]
+                    else :
+                        self.states[k][i][j] = colorTable[self.states[k][i][j].num]
+            img = plt.matshow(self.states[k],fignum=1)
+            mapsList.append([img])
+        anim = animation.ArtistAnimation(fig,mapsList,interval=100,blit=True,repeat=False)
+        plt.show()
+
     def surroundings(self,pos):
         d={0:[0,[]],1:[0,[]],2:[0,[]]}
-        l=[[pos[0],pos[1]-1],[pos[0],pos[1]+1],[pos[0]-1,pos[1]],[pos[0]+1,pos[1]]]
-        # if indice au bord, alors on remplace les valeurs de la liste par les trucs qui vont bien!
-        if pos[0]==0 :
-            l[2]=[self.width-1,pos[1]]
-        if pos[0]==self.width-1:
-            l[3]=[0,pos[1]]
-        if pos[1]==0 :
-            l[0]=[pos[0],self.width-1]
-        if pos[1]==self.width-1:
-            l[1]=[pos[0],0]
-            
-        for i in l:
-            if self.grid[i[0]][i[1]]==None:
-                d[0][0]+=1
-                d[0][1].append(i)
-            else:
-                for j in range(0,3):
-                    if self.grid[i[0]][i[1]].num==j:
-                        d[j][0]+=1
-                        d[j][1].append(i)
+        if self.grid[pos[0]][pos[1]-1]==None:
+            d[0][0]+=1
+            d[0][1].append((pos[0],pos[1]-1))
+        else:
+                for i in range(0,3):
+                    if self.grid[pos[0]][pos[1]-1].num==i:
+                        d[i][0]+=1
+                        d[i][1].append((pos[0],pos[1]-1))
+        
+        if self.grid[pos[0]][pos[1]+1]==None:
+            d[0][0]+=1
+            d[0][1].append((pos[0],pos[1]+1)) 
+        else:
+            for i in range(0,3):
+                if self.grid[pos[0]][pos[1]+1].num==i:
+                    d[i][0]+=1
+                    d[i][1].append((pos[0],pos[1]+1))
+        
+        if self.grid[pos[0]-1][pos[1]]==None:
+            d[0][0]+=1
+            d[0][1].append((pos[0]-1,pos[1])) 
+        else:
+            for i in range(0,3):
+                if self.grid[pos[0]-1][pos[1]].num==i:
+                    d[i][0]+=1
+                    d[i][1].append((pos[0]-1,pos[1]))
+        
+        if self.grid[pos[0]+1][pos[1]]==None:
+            d[0][0]+=1
+            d[0][1].append((pos[0]+1,pos[1])) 
+        else:
+            for i in range(0,3):
+                if self.grid[pos[0]+1][pos[1]].num==i:
+                    d[i][0]+=1
+                    d[i][1].append((pos[0]+1,pos[1]))
         return d
-    
-    def removeDeads(self):
-        c=0
-        for i in self.animals:
-            if type(i)==Shark and i.energy==0:
-                i.alive=False
-            if not i.alive:
-                self.animals.pop(c)
-                self.grid[i.pos[0]][i.pos[1]]=None
-            c+=1
                 
-                # for i in range(0,len(self.animals)):
-            #    if not self.animals[i].alive:
-            #      self.animals.pop(i) 
-            
+
+    def removeDeads(self):
+        pass
+        # complete here (remove pass)
 
     def tick(self):
         rd.shuffle(self.animals)
-        to_pop=[]
-        for i in range(0,len(self.animals)):
-            if self.animals[i].alive:
-                self.animals[i].rep+=1
-                d=self.surroundings(self.animals[i].pos)
-                if type(self.animals[i])==Shark:
-                    if d[1][0]>0:
-                        self.animals[i].move(self,d,1)
-                        self.animals[i].energy+=3
-                        if self.animals[i].energy>6:
-                            self.animals[i].energy=6
-                        self.animals[i].energy-=1
-                        if self.animals[i].energy==0:
-                            self.animals[i].alive=False
-                    else:
-                        self.animals[i].move(self,d,0)
-                        self.animals[i].energy-=1
-                        if self.animals[i].energy==0:
-                            self.animals[i].alive=False
-                else:
-                    self.animals[i].move(self,d,0)
-        self.removeDeads()
-                
+        # complete here
         self.states.append(copyMat(self.grid))
         self.animalCount.append(self.count())
 
@@ -135,42 +117,24 @@ class Ocean :
             else :
                 nbS += 1
         return nbF,nbS
-
+    
 class Animal :
 
     def __init__(self,x,y):
-        self.pos=(x,y)
-        self.rep=0
-        self.alive=True
-
-    def reproduce(self,ocean,oldpos):
-        self.rep=0
-        ocean.grid[oldpos[0]][oldpos[1]]=type(self)(oldpos[0],oldpos[1])
-        ocean.animals.append(ocean.grid[oldpos[0]][oldpos[1]])
+        pass
+        # complete here (remove pass)
 
     def move(self,ocean,d,cellType):
-        if d[cellType][0]==0:
-            pass
-        else:
-            a=rd.randint(0,d[cellType][0]-1)
-            oldpos=self.pos
-            if type(self)==Shark and type(ocean.grid[d[cellType][1][a][0]][d[cellType][1][a][1]])==Fish:
-                ocean.grid[d[cellType][1][a][0]][d[cellType][1][a][1]].alive=False
-                
-            ocean.grid[d[cellType][1][a][0]][d[cellType][1][a][1]]=self
-            self.pos=(d[cellType][1][a])
-            
-            ocean.grid[oldpos[0]][oldpos[1]]=None
-            if self.rep>=self.reproductionTreshold:
-                self.reproduce(ocean,oldpos)
+        pass
+        # complete here (remove pass)
 
-
-
+    def reproduce(self,ocean,oldpos):
+        pass
+        # complete here (remove pass)
 
 ###
 
 class Fish(Animal):
-
 
     num = 1
 
@@ -179,30 +143,16 @@ class Fish(Animal):
 ###
 
 class Shark(Animal):
-    
-    maximum_energy=6
-    energy_start=3
+
     num = 2
 
     reproductionTreshold = 12
-    def __init__(self,x,y):
-
-        self.energy=self.energy_start
-        super().__init__(x,y)
 
 
-nbIter = 15
-atlantic = Ocean(10,10)
+nbIter = 100
+
+atlantic = Ocean()
 atlantic.initialize()
 for _ in range(nbIter):
     atlantic.tick()
-    print(atlantic)
 atlantic.show()
-
-print("frames:", len(atlantic.states), "animals:", len(atlantic.animals), "last count:", atlantic.animalCount[-1] if atlantic.animalCount else None)
-
-
- # textual grid
-#affiche un graph vierge, wtf
-
-
